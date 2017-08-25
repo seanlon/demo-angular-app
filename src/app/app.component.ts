b456991fcdde5e3fb33608892b5a644d7744fa68
Loh Sean Lon <lsl@sitecore.net>
2017-08-25 14:29:29 +0800

    add doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+/**
+ * Hey
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   * something
+   */
   title = 'app works!';
 }
